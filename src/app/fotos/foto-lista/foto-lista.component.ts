45233ffacbb8c773ed94aa8b18055d36f0668803
ThiagoSegadilha <thiago_segadilha@hotmail.com>
2022-05-08 18:26:46 -0300

    melhorando o desenpenho do filtro com SUBJECT

--- src/app/fotos/foto-lista/foto-lista.component.ts
+++ src/app/fotos/foto-lista/foto-lista.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {Subject} from "rxjs";
+import {debounceTime} from "rxjs/operators";
+
 import {Foto} from "../foto/foto";
 
 @Component({
@@ -7,16 +10,24 @@ import {Foto} from "../foto/foto";
   templateUrl: './foto-lista.component.html',
   styleUrls: ['./foto-lista.component.css']
 })
-export class FotoListaComponent implements OnInit {
+export class FotoListaComponent implements OnInit, OnDestroy {
 
   fotos: Foto[] = [];
   filtro: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit(): void {
     this.fotos = this.activatedRoute.snapshot.data['fotos'];
+    this.debounce
+      .pipe(debounceTime(500))
+      .subscribe(filtro => this.filtro = filtro);
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
   }
 
 }
