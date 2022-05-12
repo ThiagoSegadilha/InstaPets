5a32375a5222a0da9f30e6a9d13f0a2d47798765
ThiagoSegadilha <thiago_segadilha@hotmail.com>
2022-05-11 21:04:26 -0300

    validacao de compos obrigatorios no nosso formulario

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      nomeUsuario: ['', Validators.required],
+      senha: ['', Validators.required]
+    });
   }
 
 }
